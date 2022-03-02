c2dee6a7d97dbb8c034999f3c9bfd0968f6495c6
titi_2115 <73474137+jasiukiewicztymon@users.noreply.github.com>
2022-03-02 18:46:01 +0100

    Add files via upload

--- Fun code turnament site/javascript/index.js
+++ Fun code turnament site/javascript/index.js
@@ -1,1 +1,7 @@
 // ligth and dark theme switcher
+fetch('./javascript/serverinfo.json')
+.then(res => res.json()) 
+.then(data => {
+    document.querySelector("#totalmembers").textContent = data.total;
+    document.querySelector("#activemembers").textContent = data.online;
+})
