2c588ac3f490fb5c1ba087cc75c20f817e79a7f7
titi_2115 <73474137+jasiukiewicztymon@users.noreply.github.com>
2022-03-03 23:55:24 +0100

    Create darkindex.js

---
+++ Fun code turnament site/javascript/darkindex.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    const date = new Date();
+    const hour = date.getHours();
+
+    if (hour >= 20 || hour <= 5) {
+        document.querySelector('#dark-theme').classList.add('none');
+        document.querySelector('#light-theme').classList.remove('none');
+
+        document.querySelector('body').classList.add('dark');
+        document.querySelectorAll('.github img').forEach(e => {
+            e.style.filter = 'invert(100%)';
+        })
+
+        document.getElementById('serverinfo').classList.add('darkcard');
+        document.getElementById('copyrights').classList.add('dark');
+    }
+
+    $('#dark-theme').click(() => {
+        document.querySelector('#dark-theme').classList.add('none');
+        document.querySelector('#light-theme').classList.remove('none');
+
+        document.querySelector('body').classList.add('dark');
+        document.querySelectorAll('.github img').forEach(e => {
+            e.style.filter = 'invert(100%)';
+        })
+
+        document.getElementById('serverinfo').classList.add('darkcard');
+        document.getElementById('copyrights').classList.add('dark');
+    });
+    $('#light-theme').click(() => {
+        document.querySelector('#light-theme').classList.add('none');
+        document.querySelector('#dark-theme').classList.remove('none');
+
+        document.querySelector('body').classList.remove('dark');
+        document.querySelectorAll('.github img').forEach(e => {
+            e.style.filter = 'invert(0%)';
+        })
+        
+        document.getElementById('serverinfo').classList.remove('darkcard');
+        document.getElementById('copyrights').classList.remove('dark');
+    });
+});
